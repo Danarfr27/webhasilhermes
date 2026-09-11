c29125abd2e7a1d743913e88cfe57b4153c6e661
salmanalhidamkara666 <salmanalhidamkara666@gmail.com>
2026-09-11 16:19:35 +0700

    Prepare application for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   poweredByHeader: false,
-  env: {
-    NEXT_PUBLIC_APP_NAME: "HermesProtocol",
-    NEXT_PUBLIC_APP_URL: "https://hermesprotocol.vercel.app",
-  },
 };
 
 export default nextConfig;
